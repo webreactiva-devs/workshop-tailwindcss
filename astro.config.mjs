99c36d5ed06ea434d2bc29385c3cbc77362e59de
Daniel Primo <delineas@gmail.com>
2024-10-22 22:28:26 +0200

    Add more lessons

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    syntaxHighlight: "prism"
+  },
   integrations: [tailwind()],
 });
