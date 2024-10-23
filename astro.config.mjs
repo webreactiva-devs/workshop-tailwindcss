b2c985dcbc442ad27133086e8b51f9cc60435877
Daniel Primo <delineas@gmail.com>
2024-10-23 16:32:02 +0200

    Add astro-live-code library. Add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
+import liveCode from "astro-live-code";
+import tailwind from "@astrojs/tailwind";
 
-import tailwind from '@astrojs/tailwind';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    syntaxHighlight: "prism"
+    syntaxHighlight: "prism",
   },
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    liveCode({
+      layout: "/src/components/LiveCodeLayout.astro",
+    }),
+    mdx(),
+  ],
 });
